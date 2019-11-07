4d68f597ed715909f2505b236f5b412018b9d0aa
bhanu <bhanuprathap.career1@gmail.com>
2019-11-07 15:08:24 +0530

    f

--- server/zoho-office-integrator/zoho-office-integrator.route.js
+++ server/zoho-office-integrator/zoho-office-integrator.route.js
@@ -1,10 +1,4 @@
 const express = require('express');
-// eslint-disable-next-line no-unused-vars
-const validate = require('express-validation');
-// eslint-disable-next-line import/newline-after-import
-const multer = require('multer');
-// eslint-disable-next-line no-unused-vars
-const upload = multer();
 const officeintegratorCtrl = require('./zoho-office-integrator.controller');
 
 const router = express.Router(); // eslint-disable-line new-cap
@@ -12,18 +6,5 @@ const router = express.Router(); // eslint-disable-line new-cap
 router.route('/')
   /** POST /api/users - Create new user */
   .post(officeintegratorCtrl.create);
-
-// router.route('/:userId')
-//   /** GET /api/users/:userId - Get user */
-//   .get(officeintegratorCtrl.get)
-//
-//   /** PUT /api/users/:userId - Update user */
-//   .put(validate(paramValidation.updateUser), officeintegratorCtrl.update)
-//
-//   /** DELETE /api/users/:userId - Delete user */
-//   .delete(officeintegratorCtrl.remove);
-//
-// /** Load user when API with userId route parameter is hit */
-// router.param('userId', officeintegratorCtrl.load);
 
 module.exports = router;
